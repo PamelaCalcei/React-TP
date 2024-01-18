a438f4f419e93bdb72b89bb08f140aa0e566e1a2
unknown <pecalcei@gmail.com>
2024-01-18 14:09:42 +0100

    init V2

--- reducer/vite-project/src/App.jsx
+++ reducer/vite-project/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react";
+import { todosReducer } from "./utils/reducers";
+import { todos } from "./models";
+import { CHANGE_TODO_STATUS, DELETE_TODO } from "./utils/actions";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(todosReducer, {
+    todolist: todos,
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {state.todolist.map((t) => (
+        <div key={t.id}>
+          <span
+            onClick={() =>
+              dispatch({
+                type: CHANGE_TODO_STATUS,
+                payload: {
+                  id: t.id,
+                  done: !t.done,
+                },
+              })
+            }
+          >
+            {t.txt}
+          </span>{" "}
+          - {t.done ? "FAIT" : "PAS FAIT"}
+          <button
+            onClick={() =>
+              dispatch({
+                type: DELETE_TODO,
+                payload: t.id,
+              })
+            }
+          >
+            Supprimer
+          </button>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
